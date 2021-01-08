447185534b6b6e206a50cc10ebddfb8f3f2d6258
thauandev <thauandepaula@icloud.com>
2021-01-08 11:16:24 -0300

    Listagem, Adição no carrinho

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,26 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const produtcsSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + produtcsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
 
-    return 0;
+    const total = products.reduce((accumulator, product) => {
+      const produtcsQuantity = product.quantity;
+
+      return accumulator + produtcsQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
